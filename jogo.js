7d51749dc3d4a66884a79fd2fbe8ea01d65a57f7
Maikel <maikelmysilvestre@gmail.com>
2022-06-28 14:43:50 -0300

    adicionando pontuacao

--- jogo.js
+++ jogo.js
@@ -1,6 +1,10 @@
 const mario = document.querySelector('#mario');
 const pipe = document.querySelector('#pipe');
 const clouds = document.querySelector('#clouds')
+const gameScreen = document.querySelector('#game')
+const carabom = document.querySelector('#carabom')
+let score = document.querySelector('#points')
+let points = 0;
 
 const jump = () => {
   if(mario.classList.length === 0) {
@@ -14,8 +18,14 @@ const jump = () => {
 const verify = setInterval(() => {
   const pipePosition = pipe.offsetLeft;
   const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+  points ++;
+  score.innerText = points;
   // o + antes do window converte a string resultante para numero
   const cloudsPosition = clouds.offsetLeft;
+  points > 500 && points < 1500 ? carabom.innerText = 'Que cara bom !!' 
+  : points >= 1500 ? carabom.innerText = 'Tá bom amigo! Já platinou o game xD !!' 
+  : carabom.innerText = 'Corre !!!'
+  
 
   if (pipePosition <= 95 && pipePosition > -30 && marioPosition <= 85) {
     pipe.style.animation = 'none';
@@ -29,3 +39,4 @@ const verify = setInterval(() => {
 }, 10)
 
 document.addEventListener('keydown', jump);
+gameScreen.addEventListener('click', jump);
